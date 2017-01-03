689d34fff3160b5d40b44d701715d23e2af1c358
mohit chattlani <chattlanimohit@gmail.com>
2017-01-03 18:50:36 +0530

    Corrected the spelling error

--- public/about.php
+++ public/about.php
@@ -8,7 +8,7 @@
 			<p class="para">A family clothing shop based in New delhi.We deal in formal clothing,casual clothing
 		,woolen clothing , baby clothing ,wedding clothing and other clothing accessories.The store is open all seven days with working hours from  10:30AM to 9:30PM <kbd>except Monday.</kbd></p>
 		<p class="para">
-		The store was started almost <code>25</code> years ago by valid contributions from all the family members starting from eldest to the youngest member . Hardwork and patience has lead us here . The <kbd> motive</kbd> of the store is to provide good quality clothes for all age-groups and that too at affordrable prices.</p>
+		The store was started almost <code>25</code> years ago by valid contributions from all the family members starting from eldest to the youngest member . Hardwork and patience has led us here . The <kbd> motive</kbd> of the store is to provide good quality clothes for all age-groups and that too at affordrable prices.</p>
 		<p class="para">
 		Our customers mean everything to us and so we thank all our customers for shopping with us ! We hope you will come and visit us again .
 		</p>
