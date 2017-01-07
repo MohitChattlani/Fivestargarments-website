5d84415f71fca873133c909c4efb70945cd4fd1c
mohit chattlani <chattlanimohit@gmail.com>
2017-01-07 17:48:50 +0530

    Added navbar for better viewing on mobile

--- templates/header.php
+++ templates/header.php
@@ -8,20 +8,41 @@
 	<link rel="stylesheet" type="text/css" href="stylesheet/style.css"/>
 	<link rel="shortcut icon" href="images/FSfront2.jpg"/>
 	<link rel="stylesheet" type="text/css" href="stylesheet/bootstrap.min.css"/>
-	<link href="https://fonts.googleapis.com/css?family=Arima+Madurai|Raleway|Reem+Kufi" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<!-- for changing the class to active when clicked-->
+  	<script>
+   $(function(){
+		$(".nav li").on("click",function(){
+		  $(".nav li").removeClass("active");
+		  $(this).addClass("active");
+		});
+	});
+	</script>
+	<link href="https://fonts.googleapis.com/css?family=Arima+Madurai|Raleway|Reem+Kufi" rel="stylesheet" />
 	<title><?=htmlspecialchars($title) ?></title>
 </head>
 <body>
-<div class="container">
-	<div class="row" align="center">
-		<a href="index.php"><img src="images/newfront.jpg" width="140px" height="140px" /></a>
-	</div>
-	<div class="row">
-		<div class="btn-group btn-group-md"></div>
-		<ul>
-		<div class="col-sm-3"><li><a href="index.php" class="btn btn-default"><span class="glyphicon glyphicon-home"></span> HOME</a></li></div>
-		<div class="col-sm-3"><li><a href="gallery.php" class="btn btn-default"><span class="glyphicon glyphicon-film"></span> GALLERY</a></li></div>
-		<div class="col-sm-3"><li><a href="about.php" class="btn btn-default"> <span class="glyphicon glyphicon-log-out"></span> ABOUT US</a></li></div>
-		<div class="col-sm-3"><li><a href="contact.php" class="btn btn-default"><span class="glyphicon glyphicon-search"></span> FIND US</a></li></div>
-		</ul>
-	</div>
+<div class="row" align="center">
+		<a href="index.php"><img src="images/newfront.jpg" width="120px" height="120px" /></a>
+</div>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="pull-left navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">Five star garments</a>
+     </div> 
+    <div class="collapse navbar-collapse" id="myNavbar">
+    	<ul class="nav navbar-nav">
+      	<li class="active"><a href="index.php"><span class="glyphicon glyphicon-home"></span> HOME</a></li>
+      	<li><a href="gallery.php"><span class="glyphicon glyphicon-film"></span> GALLERY</a></li>
+      	<li><a href="about.php"><span class="glyphicon glyphicon-log-out"></span> ABOUT US</a></li>
+      	<li><a href="contact.php"><span class="glyphicon glyphicon-search"></span> FIND US</a></li>
+    	</ul>
+    </div>	
+  </div>
+</nav>
