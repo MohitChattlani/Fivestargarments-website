36f0dfa2abacbf2f474eda7c67846d64aee35840
mohit chattlani <chattlanimohit@gmail.com>
2017-05-07 19:51:56 +0530

    added dropdown functionality.

--- templates/header.php
+++ templates/header.php
@@ -13,6 +13,11 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <title><?=htmlspecialchars($title) ?></title>
+  <script type="text/javascript">
+  $(document).ready(function() {
+    $(".dropdown-toggle").dropdown();
+    });
+  </script>
 </head>
 <body>
 <div class="row" align="center">
